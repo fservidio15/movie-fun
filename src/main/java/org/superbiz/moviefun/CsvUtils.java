7982b5fa372252c67be9a7b4b65f89aa9c8b89a5
Andy <andrewcunnell@hotmail.co.uk>
2019-07-22 15:33:09 +0100

    Removed reads from file system

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -13,18 +13,18 @@ import java.util.Scanner;
 public class CsvUtils {
 
     public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
 
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
+        ClassLoader classLoader = CsvUtils.class.getClassLoader();
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        Scanner scanner = new Scanner(classLoader.getResourceAsStream(path)).useDelimiter("\\A");
+
+        if (scanner.hasNext()) {
+            return scanner.next();
+        } else {
+            return "";
         }
+
+
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
